e65da2b32ca18bc99240c35a7ef4946bc8ca91b3
JoaquinSabater <joaquinsabater15@gmail.com>
2025-03-10 15:15:23 -0300

    Initial commit

---
+++ app/ui/cellphonefree-logo.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function CellPhoneFreeLogo() {
+  return (
+    <div
+      className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
+    >
+      <Image src="/CellPhoneFreeLogos/cellphonesinfondo.png" alt="Company Logo" width={88} height={88} />
+    </div>
+  );
+}
